8069271bc013f082c56a9107f8b68f8efe461d42
CIKADA <adel.garipovazn@mail.ru>
2023-02-25 18:59:32 +0300

    Refactor logic

--- js/app.js
+++ js/app.js
@@ -3,51 +3,44 @@ const App = {
     return {
       title: "Programmer memes",
       subTitle: "Make programmer memes great again!",
-      memes: [
-        {
-          id: 1,
-          title: "Developing",
-          name: "joke-drake",
-          tags: ["Frontend", "Backend"],
-          imagePath: "pic1.jpg",
-        },
-        {
-          id: 2,
-          title: "JavaScript Programmer",
-          name: "nazim",
-          tags: ["Javascript", "Ruby on Rails"],
-          imagePath: "pic2.jpg",
-        },
-        {
-          id: 3,
-          title: "Developing",
-          name: "simpson",
-          tags: ["Frontend", "Backend"],
-          imagePath: "pic3.jpg",
-        },
-        {
-          id: 4,
-          title: "Php developer",
-          name: "google",
-          tags: ["Php"],
-          imagePath: "pic4.jpg",
-        },
-        {
-          id: 5,
-          title: "Programmer",
-          name: "cat",
-          tags: ["coding"],
-          imagePath: "pic5.jpg",
-        },
-        {
-          id: 6,
-          title: "UNIX",
-          name: "pingvin",
-          tags: ["linux"],
-          imagePath: "pic6.jpg",
-        },
-      ],
+      allMemes: [],
+      currentUserMemes: [],
     };
+  },
+  async created() {
+    await this.fetchMemes();
+    this.generateMemes();
+  },
+  methods: {
+    async fetchMemes() {
+      response = await fetch("./db/db.json", {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      data = await response.json();
+      // console.log("data: ", data.memes);
+      this.allMemes = data.memes;
+    },
+    generateMemes() {
+      const chosenMemesId = new Set();
+      const chosenMemes = new Set();
+      const allMemesId = this.allMemes.map((item) => item.id);
+      while (chosenMemesId.size !== 6) {
+        let randomInteger = this.getRandomInt(0, this.allMemes.length - 1);
+        if (!chosenMemesId.has(this.allMemes[randomInteger].id)) {
+          chosenMemesId.add(this.allMemes[randomInteger].id);
+          chosenMemes.add(this.allMemes[randomInteger]);
+        }
+      }
+      const arrayChosenMemes = [...chosenMemes];
+      // console.log("arrayChosenMemes: ", arrayChosenMemes);
+      this.currentUserMemes = arrayChosenMemes;
+    },
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
   },
 };
 
